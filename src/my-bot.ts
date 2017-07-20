93601fcb20bc5aa43cc7524645af0505da46d676
Noel Loo <Noel.Loo@softwire.com>
2017-07-20 10:48:49 +0100

    test logging

--- src/my-bot.ts
+++ src/my-bot.ts
@@ -14,6 +14,7 @@ export class MyBot {
         if(previousShot) {
             return this.getNextTarget(previousShot.Position);
         }
+        console.log(gamestate);
         return { Row: "A", Column: 1 };  
     }
 
